93e691f4f20c79b9b9a496e499f181cdac32fd8a
Guocork <g1024536444@gmail.com>
2024-09-08 19:06:47 +0800

    分离符号位、指数、尾数

--- src/endianness.rs
+++ src/endianness.rs
@@ -21,11 +21,40 @@ mod test {
         println!("0xAABBCCDD in decimal is: {}", hex_value);  // 果然 整数在计算机中是以小端序来进行存储的
     }
 
-    // >> 位移操作符
+    // >> 位移操作符 提取符号位
     #[test]
     fn test1() {
         let n: f32 = 42.42;
         let n_bits: u32 = n.to_bits();
         let sign_bits = n_bits >> 31;
+    }
+
+    // 提取指数 从一个f32中分离并解码出指数
+    #[test]
+    fn test2() {
+        let n: f32 = 42.42;
+        let n_bits: u32 = n.to_bits();
+        let exponent_ = n_bits >> 23;
+        let exponent_ = exponent_ & 0xff;
+        let exponent = (exponent_ as i32) - 127;
+    }
+
+    // 分离解码尾数
+    #[test]
+    fn test3() {
+        let n: f32 = 42.42;
+        let n_bits: u32 = n.to_bits();
+        let mut mantissa: f32 = 1.0;
+
+        for i in 0..23 {
+            let mask = 1 << i;
+            let one_at_bit_i = n_bits & mask;
+            if one_at_bit_i != 0 {
+                let i_ = i as f32;
+                let weight = 2_f32.powf(i_ - 23.0);
+                mantissa += weight;
+            }
+        }
+        println!("{}", mantissa);
     }
 }
